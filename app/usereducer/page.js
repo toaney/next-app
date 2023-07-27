a3c8e5eadecf38a6de4e00ce213c7f21bcbc2ec7
toaney <toaney@gmail.com>
2023-07-27 13:01:54 -0700

    build counter with useReducer

--- app/usereducer/page.js
+++ app/usereducer/page.js
@@ -3,26 +3,61 @@
 import React, {useReducer, useState} from 'react'
 import Todo from './todo'
 
-function reducer() {
-
+const ACTIONS = {
+  INCREMENT: 'increment', 
+  DECREMENT: 'decrement'
 }
 
-const page = (() => {
-  const [state, dispatch] = useReducer(reducer, {todos:[]})
-  const [input, setInput] = useState('')
 
-
-  function handleSubmit(e){
-    e.prevent.default
+function reducer(state, action) {
+  switch ( action.type ) {
+    case ACTIONS.INCREMENT: 
+      return { count: state.count + 1};
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1};
+    default: 
+      return state;
   }
+};
+
+const page = (() => {
+  const [state, dispatch] = useReducer(reducer, {count: 0});
+  // const [input, setInput] = useState('')
+  const [count, setCount] = useState(0);
+
+
+  // function handleSubmit(e){
+  //   e.prevent.default
+  // };
+
+  function increment(){
+    // setCount(count + 1);
+    dispatch({type: ACTIONS.INCREMENT});
+  };
+
+  function decrement(){
+    // setCount(count - 1);
+    dispatch({type: ACTIONS.DECREMENT});
+
+  };
 
   return (
     <>
       <h1>useReducer</h1>
-      <form onSubmit={handleSubmit}>
+      <button onClick={decrement}>-</button>
+      <span> &nbsp;{state.count} &nbsp;</span>
+      <button onClick={increment}>+</button>
+
+
+
+
+
+
+
+      {/* <form onSubmit={handleSubmit}>
         <input type="text" value={input} onChange={e => setInput(e.target.value)} />
-      </form>
-      <Todo />
+      </form> */}
+      {/* <Todo /> */}
       {/* {todos.map(todo => {
 
       })} */}
