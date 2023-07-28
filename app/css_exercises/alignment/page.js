2865bfee455c1459c93444fa064d8bfd8c0ec3ec
toaney <toaney@gmail.com>
2023-07-28 10:39:13 -0700

    add css alignment page

---
+++ app/css_exercises/alignment/page.js
@@ -0,0 +1,20 @@
+import './page.css';
+
+export default function page () {
+  return (
+    <>
+      <h3>CSS Alignment</h3>
+
+      <div className="container">
+        <div className="child1"></div>
+        <div className="child2"></div>
+        <div className="child3"></div>
+        <div className="child4"></div>
+        <div className="child5"></div>
+        <div className="child6"></div>
+        <div className="child7"></div>
+      </div>
+    </>
+  )
+}
+
