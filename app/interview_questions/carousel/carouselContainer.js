1a1629c6393a89a2f7891f4ffde6f48f53bb41e0
toaney <toaney@gmail.com>
2023-08-02 12:56:23 -0700

    add carousel page

--- app/interview_questions/carousel/carouselContainer.js
+++ app/interview_questions/carousel/carouselContainer.js
@@ -4,15 +4,17 @@ import React from 'react'
 import CarouselItem from './carouselItem';
 
 export default function CarouselContainer({items}) {
-  const[itemList, useItemList] = React.useState(["a", "b", "c"])
+  const[itemList, useItemList] = React.useState(items)
   const[activeIndex, setActiveIndex] = React.useState(0);
 
   function updateIndex (newIndex){
     if (newIndex < 0) { 
-      setActiveIndex(0)
+      // setActiveIndex(0)
+      newIndex = 0
     }
-    if (newIndex > items.length){
-      setActiveIndex(items.length - 1)
+    if (newIndex >= itemList.length){
+      // setActiveIndex(itemList.length - 1)
+      newIndex = itemList.length - 1;
     }
     setActiveIndex(newIndex)
   }
@@ -23,7 +25,8 @@ export default function CarouselContainer({items}) {
       <h3>Carousel Container</h3>
       <div 
         className="inner"
-        style={{ transform: `translate:(-${activeIndex * 100}%)`}}
+        // style={{ transform: `translate:(-${activeIndex * 100}%)`}}
+        style={{ transform: `translate(-${activeIndex * 100}%)`}}
       >
         {items.map((item, index) => {
           return(
@@ -33,12 +36,13 @@ export default function CarouselContainer({items}) {
       </div>
       <div>
         <button onClick={() => (updateIndex(activeIndex - 1))}>prev</button>
-        {items.map(({items, index}) => {
-          return (<button onClick={() => (updateIndex(index))}>.</button>)
+        {itemList.map((item, index) => {
+          return (<button onClick={() => (updateIndex())}>.</button>)
         })}
 
-        <button onClick={() => (updateIndex(activeIndex + 1))}>prev</button>
+        <button onClick={() => (updateIndex(activeIndex + 1))}>next</button>
       </div>
+      <div style={{color:"white"}}>{activeIndex}</div>
     </div>
   )
 }
--- app/interview_questions/carousel/page.js
+++ app/interview_questions/carousel/page.js
@@ -11,19 +11,29 @@ import "./carousel.css";
 // style components
 const list = [
   {
-    title: "Title 1", 
+    title: "Title 0", 
     image: "https://placebear.com/200/300",
-    description: "Title 1: Placebear 200 x 300"
+    description: "Title 0: Placebear 200 x 300"
+  },
+  {
+    title: "Title 1", 
+    image: "https://placebear.com/300/300",
+    description: "Title 1: Placebear 300 x 300"
   },
   {
     title: "Title 2", 
-    image: "https://placebear.com/300/300",
-    description: "Title 2: Placebear 300 x 300"
+    image: "https://placebear.com/300/200",
+    description: "Title 2: Placebear 300 x 200"
   },
   {
     title: "Title 3", 
     image: "https://placebear.com/300/200",
     description: "Title 3: Placebear 300 x 200"
+  },
+  {
+    title: "Title 4", 
+    image: "https://placebear.com/300/200",
+    description: "Title 4: Placebear 300 x 200"
   }
 ]
 
