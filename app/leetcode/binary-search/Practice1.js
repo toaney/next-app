99b4848d70f71ac376744744df783f687a422182
toaney <toaney@gmail.com>
2023-08-17 16:29:12 -0700

    add binary search

---
+++ app/leetcode/binary-search/Practice1.js
@@ -0,0 +1,36 @@
+'use client';
+
+export default function Practice1(){
+  console.log("practice 1")
+  // let array = [1, 4, 6, 3, 7, 3, 9, 2, 5, 8];
+  // let target = 4;
+
+  function findNumIndex(array, target){
+    let left = array[0];
+    let right = array[array.length - 1];
+    let mid;
+
+    while (left !== right) {
+      let mid = Math.floor((left + right) / 2)
+
+      if (array[mid] === target){
+        return array[mid];
+      };
+
+      if (target < array[mid]){
+        right = mid;
+        mid = Math.floor((mid / 2) - 1) 
+      } else {
+        left = mid;
+        mid = mid + Math.floor((right - mid) / 2)
+      }
+
+    }
+
+  }
+
+
+  findNumIndex([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)
+
+  return <h4>Practice 1</h4>
+}
---
+++ app/leetcode/binary-search/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+import Practice1 from './Practice1';
+
+export default function page(){
+  return(
+    <>
+      <h2>Binary Search</h2>
+      <ul>
+        <li>input is an array</li>
+      </ul>
+      <Practice1 />
+    </>
+  );
+}
