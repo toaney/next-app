cd43779caa2d7b49197b1c86bed8a0ab77a0d098
toaney <toaney@gmail.com>
2023-08-29 14:24:25 -0700

    add hacker rank folder and substring-with-most-vowels page

---
+++ app/hackerrank/substring-with-most-vowels/page.js
@@ -0,0 +1,8 @@
+'use client'
+
+const page = () => {
+  return (
+    <h2>Substring with most vowels</h2>
+    <p>given substring s and integer k, return the starting index of the substring with th emost vowels. If no substring exists, return "Not Found!" witout the quotation marks.</p>
+  )
+}
