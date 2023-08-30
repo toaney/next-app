ad86de05ae443339428a5641fde084edef231388
toaney <toaney@gmail.com>
2023-08-30 11:07:21 -0700

    add table filters

--- app/components/table/page.js
+++ app/components/table/page.js
@@ -1,91 +1,109 @@
 'use client';
 
-import React, {useEffect, useState} from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './styles.css';
 
-let columnNames = [
-  {value: 'firstName', label: "First Name"},
-  {value: 'lastName', label: "Last Name"},
-  {value: 'email', label: "Email"}
-  
-]
 
-const Header = ({columns}) => {
-  console.log('********')
-  console.log(columns)
+function sortData({ tableData, sortKey, sortOrder }) {
 
-  return(
+  if (!sortKey) return tableData;
 
-      <thead>
-        <tr>
-          {columns.map(column=>{
-            return(
-              <td>{column.label}</td>
-            )
-          })
+  const sortedData = tableData.sort((a, b) => {
+    return a[sortKey] < b[sortKey] ? -1 : 1;
+  })
 
-          }
+  if (sortOrder === 'desc') {
+    return sortedData.reverse();
+  }
 
-        </tr>
-      </thead>
-
-  )
+  return sortedData
 }
 
-const Body = ({data}) => {
+function SortButton({columnValue, onClick, sortOrder, sortKey} ) {
 
-  console.log("######")
-  console.log(data)
-  return(
-    <tbody>
-      <tr>
-        <td>data1</td>
-        <td>data2</td>
-        <td>data3</td>
-      </tr>
-      {data.map(item => {
-        return(
-          <tr>
-            <td>{item.firstName}</td>
-            <td>{item.lastName}</td>
-            <td>{item.email}</td>
-          </tr>
-        
-        )
-      })}
-    </tbody>
+  return (
+    <button onClick={onClick}>
+      {/* {console.log(columnValue)} */}
+      {sortKey === columnValue && sortOrder === 'asc' ?
+        '▲'
+        :
+        'V'
+      }
+      {/* {columnValue} */}
+    </button>
   )
 }
-
 
 const table = () => {
   const [users, setUsers] = useState([]);
+  const [sortKey, setSortKey] = useState('firstName');
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  let columnNames = [
+    { value: 'firstName', label: "First Name" },
+    { value: 'lastName', label: "Last Name" },
+    { value: 'email', label: "Email" }
+  ]
+
+  const sortedData = useCallback(
+    () => sortData({ tableData: users, sortKey, sortOrder }),
+    [users, sortKey, sortOrder]
+  );
+
+  function changeSort(key){
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    setSortKey(key);
+  }
 
   useEffect(() => {
     let unmounted = false;
 
     fetch('https://dummyjson.com/users')
-    .then(res => res.json())
-    .then(json => {
-      console.log(json)
-      if (!unmounted) {
-        setUsers(json.users)
-      }
-    })
-      
-    return () => {unmounted = true};
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        if (!unmounted) {
+          setUsers(json.users)
+        }
+      })
+
+    return () => { unmounted = true };
   }, [])
-
-
-
 
 
   return (
     <>
       <h2>Table</h2>
       <table className='table-component'>
-        <Header columns={columnNames}/>
-        <Body data={users}/>
+        <thead>
+          <tr>
+            {columnNames.map(column => {
+              {console.log('$$$$$$$$$$$$$$')}
+              {console.log(column.value)}
+              return (
+                <td>{column.label} <SortButton columnValue={column.value} onClick={() => changeSort(column.value)} sortOrder={sortOrder} sortKey={sortKey}/></td>
+              )
+            })
+
+            }
+
+          </tr>
+        </thead>
+
+        <tbody>
+          {/* {console.log('@@@@@@@@@@@@@@@@@@@@@')}
+      {console.log(sortedData)} */}
+          {sortedData().map(user => {
+            return (
+              <tr>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+              </tr>
+
+            )
+          })}
+        </tbody>
       </table>
     </>
   )
