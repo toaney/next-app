5d70cc3e4aa8ec32df935193d1ff59ea01312344
toaney <toaney@gmail.com>
2023-09-05 23:29:13 -0700

    longest substring work

--- app/hackerrank/substring-with-most-vowels/LongestSubstring.js
+++ app/hackerrank/substring-with-most-vowels/LongestSubstring.js
@@ -4,31 +4,47 @@ const LongestSubstring = () => {
 
 
   function findSubstring(s, k){
-    let longestSubstringIndex = 0;
+    let substringIndex = 0;
     let mostVowels = 0;
-    let mostVowelsStart = 0
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let start = 0;
-    let soFar = '';
-    let soFarVowelCount;
+    let soFar = 0;
+
 
     for (let end = 0; end < s.length; end++){
-      soFar = soFar + s[end];
+      console.log(s[end])
+
+
+      if (vowels.includes(s[end])){
+        soFar += 1
+      }
 
       if (end - start === k - 1){
-        if (vowels.includes(s[end])){
-          soFarVowelcount += 1
-
-          
+        if (vowels.includes(s[start])){
+          soFar -= 1
         }
 
+  
         // helper function to check how many vowels are in the current string
+            
+        if (soFar > mostVowels){
+          mostVowels = soFar;
+          substringIndex = start;
+        }
+
+        start++
       }
+
+      
+      console.log(soFar)
     }
+
+    console.log(`max index: ${substringIndex}, max vowels: ${mostVowels}`)
+    return 
   }
 
 
-  findSubstring('alkbjoeisfeewoagioe', 5);
+  findSubstring('aeiou', 3);
 
   return(
     <>
