da381701f6343ac343658e410d262deec7275711
toaney <toaney@gmail.com>
2023-10-11 12:11:26 -0700

    valid parentheses practice

---
+++ app/leetcode/valid-parentheses/Practice4.js
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react';
+
+const Practice4 = () => {
+
+  const validateParens = (s) => {
+    let stack = []
+
+    let validParens = ' () {} []';
+
+    // main loop
+    for (let i = 0; i < s.length; i++) {
+
+      stack.push(s[i]);
+      console.log(stack)
+
+      let potentialparens = stack[stack.length - 2] + stack[stack.length -1]
+      console.log("potential parens")
+      console.log(potentialparens)
+
+
+      if ( validParens.includes(potentialparens)) {
+        stack.pop()
+        stack.pop()
+      }
+
+    }
+
+    return stack.length? false : true
+  }
+
+
+  console.log(validateParens('({[])'))
+
+
+  return <>
+    <p>Practice 4</p>
+  </>
+}
+
+export default Practice4;
--- app/leetcode/valid-parentheses/page.js
+++ app/leetcode/valid-parentheses/page.js
@@ -3,6 +3,7 @@
 import Practice1 from './Practice1';
 import Practice2 from './Practice2';
 import Practice3 from './Practice3';
+import Practice4 from './Practice4';
 
 import React from 'react'
 
@@ -11,7 +12,7 @@ const page = () => {
   return (
     <>
       <h4>Parens</h4>
-      <Practice3 />
+      <Practice4 />
     </>
   )
 }
